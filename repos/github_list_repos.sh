660b88e4d7c0595baa382ae8cd1307b95af1a0a1
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-12-05 13:02:45 +0100

    Download full repo list

--- repos/github_list_repos.sh
+++ repos/github_list_repos.sh
@@ -1,2 +1,8 @@
-wget -O repos.json https://api.github.com/users/Artsdatabanken/repos?per_page=150
-fx repos.json 'x=>x.map(e=>e.name)' >repo_names.json
+wget -O part1.json https://api.github.com/user/17045522/repos?per_page=100&page=1
+wget -O part2.json https://api.github.com/user/17045522/repos?per_page=100&page=2
+wget -O part3.json https://api.github.com/user/17045522/repos?per_page=100&page=3
+jq -s . part*.json >all_repos.json
+
+fx all_repos.json 'x=>x.reduce((acc,e)=>{acc.push(...e);return acc},[]).filter(e=>!e.archived).map(e=>e.name).sort((a,b)=>a>b?1:-1)' >repo_names.json
+
+#fx repos.json 'x=>x.filter(e=>!e.archived).map(e=>e.name)' >repo_names.json
