d76e02cf018cec3337a54f034586f44afa5e3079
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-12-05 15:06:03 +0100

    Use Array flat

--- repos/github_list_repos.sh
+++ repos/github_list_repos.sh
@@ -3,6 +3,4 @@ wget -O part2.json https://api.github.com/user/17045522/repos?per_page=100&page=
 wget -O part3.json https://api.github.com/user/17045522/repos?per_page=100&page=3
 jq -s . part*.json >all_repos.json
 
-fx all_repos.json 'x=>x.reduce((acc,e)=>{acc.push(...e);return acc},[]).filter(e=>!e.archived).map(e=>e.name).sort((a,b)=>a>b?1:-1)' >repo_names.json
-
-#fx repos.json 'x=>x.filter(e=>!e.archived).map(e=>e.name)' >repo_names.json
+fx all_repos.json 'x=>x.flat().filter(e=>!e.archived).map(e=>e.name).sort((a,b)=>a>b?1:-1)' >repo_names.json
